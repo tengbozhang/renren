801d4ff833305a4403f1a7acb910a4136bf4d9d7
tengbozhang <tengbozhang@gmail.com>
2018-11-12 14:21:50 +0800

    modified:   renren/settings.py 	new file:   requirements.txt

--- renren/settings.py
+++ renren/settings.py
@@ -8,28 +8,10 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-import random
-# user agent 列表
-USER_AGENT_LIST = [
-    'MSIE (MSIE 6.0; X11; Linux; i686) Opera 7.23',
-    'Opera/9.20 (Macintosh; Intel Mac OS X; U; en)',
-    'Opera/9.0 (Macintosh; PPC Mac OS X; U; en)',
-    'iTunes/9.0.3 (Macintosh; U; Intel Mac OS X 10_6_2; en-ca)',
-    'Mozilla/4.76 [en_jp] (X11; U; SunOS 5.8 sun4u)',
-    'iTunes/4.2 (Macintosh; U; PPC Mac OS X 10.2)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0) Gecko/20100101 Firefox/5.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20120813 Firefox/16.0',
-    'Mozilla/4.77 [en] (X11; I; IRIX;64 6.5 IP30)',
-    'Mozilla/4.8 [en] (X11; U; SunOS; 5.7 sun4u)'
-]
-# 随机生成user agent
-USER_AGENT = random.choice(USER_AGENT_LIST)
-DEFAULT_REQUEST_HEADERS = {
-    'Referer': 'http://www.zimuzu.tv',
-     'User-agent':USER_AGENT
 
-}
+# user agent 列表
+
+
 BOT_NAME = 'renren'
 RENREN_USERNAME = ""
 RENREN_PASSWORD = ""
@@ -71,7 +53,10 @@ DOWNLOAD_DELAY = 0.1
 #SPIDER_MIDDLEWARES = {
 #    'renren.middlewares.RenrenSpiderMiddleware': 543,
 #}
-
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+}
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
